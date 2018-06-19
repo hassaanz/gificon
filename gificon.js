d794f9013021385073999ab4318ecf52bc5f7b4e
Hassaan Zaidi <hassaanz@gmail.com>
2018-06-20 04:57:14 +0500

    Initial prototype

---
+++ gificon.js
@@ -0,0 +1,143 @@
+document.head = document.head || document.getElementsByTagName('head')[0];
+
+function clearNode(node) {
+  while (node.firstChild) {
+    node.removeChild(node.firstChild);
+  }
+}
+
+
+function changeFavicon(src) {
+  const link = document.createElement('link');
+  const oldLink = document.getElementById('dynamic-favicon');
+  link.id = 'dynamic-favicon';
+  link.rel = 'shortcut icon';
+  link.type = 'image/png';
+  link.href = src;
+  if (oldLink) {
+    document.head.removeChild(oldLink);
+  }
+  document.head.appendChild(link);
+}
+
+function onReady() {
+
+
+  function getGifIconVals(rub) {
+    const TOTAL_FRAMES = 1024;
+    if (!rub) {
+      return;
+    }
+    const total = rub.get_length();
+    if (total > TOTAL_FRAMES) {
+
+    }
+  }
+
+  function onImageUpload(ev) {
+    if (!this.files.length) {
+      return;
+    }
+    let playing = false;
+    const previewContainer = document.querySelector('.file-preview');
+    const uploaded = ev.target.files[0];
+    const uploadedURL = window.URL.createObjectURL(uploaded);
+    const imgPreview = document.createDocumentFragment();
+    const img = document.createElement('img');
+    img.src = uploadedURL;
+    clearNode(previewContainer);
+    imgPreview.appendChild(img);
+    previewContainer.appendChild(imgPreview);
+
+    const rub = new SuperGif({
+      gif: img,
+      loop_mode: true,
+      auto_play: false,
+      progressbar_height: 0
+      // max_width: 32,
+    });
+    console.log(rub);
+    rub.pause();
+    rub.load(function onGifLoad(imgEl, cvs) {
+      function step() {
+        rub.move_relative(1);
+        const canvasScreenshotURI = canvas.toDataURL();
+        changeFavicon(canvasScreenshotURI);
+        if (playing) {
+          window.requestAnimationFrame(step);
+        }
+      }
+      const canvas = rub.get_canvas();
+      // PLay / Pause GIF on click
+      canvas.addEventListener('click', () => {
+        playing = !playing;
+        if (playing) {
+          rub.play();
+          step();
+        } else {
+          rub.pause();
+        }
+      });
+      const canvasScreenshotURI = canvas.toDataURL();
+      changeFavicon(canvasScreenshotURI);
+
+      if (playing) {
+        window.requestAnimationFrame(step);
+      }
+    });
+  }
+
+  function getFilesFromDrop(ev) {
+    const files = [];
+    if (!ev.dataTransfer.items) {
+      // Use DataTransfer interface to access the file(s)
+      return ev.dataTransfer.files;
+    }
+    // Use DataTransferItemList interface to access the file(s)
+    for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+      // If dropped items aren't files, reject them
+      if (ev.dataTransfer.items[i].kind === 'file') {
+        var file = ev.dataTransfer.items[i].getAsFile();
+        files.push(files);
+      }
+    }
+  }
+
+  setTimeout(() => {
+    const previewContainer = document.querySelector('.file-preview');
+    const defaultStyle = previewContainer.style;
+    function onDragEnter() {
+      previewContainer.style.opacity = 0.7;
+      previewContainer.style.backgroundColor = '#ccc';
+      previewContainer.style.border = 'dashed';
+    }
+    function onDragLeave() {
+      previewContainer.style = defaultStyle;
+    }
+    function onDrop(ev) {
+      ev.preventDefault();
+      const files = getFilesFromDrop(ev);
+      console.log(files);
+      // Pass event to removeDragData for cleanup
+      removeDragData(ev)
+    }
+    const fileUploader = document.querySelector('input#gificon');
+    previewContainer.addEventListener('dragenter', onDragEnter, false);
+    previewContainer.addEventListener('dragleave', onDragLeave, false);
+    previewContainer.addEventListener('ondrop', onDrop, false);
+    previewContainer.ondragover = (ev) => {
+
+    }
+    previewContainer.addEventListener('click', () => {
+      if (fileUploader) {
+        fileUploader.click();
+      }
+    }, false);
+    if (fileUploader) {
+      fileUploader.addEventListener('change', onImageUpload);
+    }
+  }, 200);
+
+}
+
+window.onload = onReady();
